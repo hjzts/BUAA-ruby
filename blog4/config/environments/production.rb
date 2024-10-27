a73c1ab3fb57cf6eb2437478d773066d7955ffbd
hjzts <119279415+hjzts@users.noreply.github.com>
2024-10-28 07:17:02 +0800

    ♻refactor:/测试blog功能

--- blog4/config/environments/production.rb
+++ blog4/config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << "22373051.blog4.rubyapp.act.buaa.edu.cn:9000"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
