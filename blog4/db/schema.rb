b0955024d3419e41f44c3358660d4d0f46963155
hjzts <119279415+hjzts@users.noreply.github.com>
2024-12-10 22:23:51 +0800

    finish blog

--- blog4/db/schema.rb
+++ blog4/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_27_223408) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_10_132235) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -26,5 +26,28 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_27_223408) do
     t.index ["blog_id"], name: "index_comments_on_blog_id"
   end
 
+  create_table "followships", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "following_user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["following_user_id"], name: "index_followships_on_following_user_id"
+    t.index ["user_id"], name: "index_followships_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   add_foreign_key "comments", "blogs"
+  add_foreign_key "followships", "users"
+  add_foreign_key "followships", "users", column: "following_user_id"
 end
