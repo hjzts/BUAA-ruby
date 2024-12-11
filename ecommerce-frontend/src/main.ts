450d7e11bc14cf907870f46f5573ed2c6714bbcf
hjzts <119279415+hjzts@users.noreply.github.com>
2024-12-11 20:01:06 +0800

    rocket🚀:/先建立User模型

---
+++ ecommerce-frontend/src/main.ts
@@ -0,0 +1,32 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+// vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+// Element Plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+app.use(ElementPlus)
+
+app.mount('#app')
