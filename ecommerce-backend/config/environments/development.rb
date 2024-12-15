fd4a580bedcef41ddf1cdc46292d64b7e05aee93
hjzts <119279415+hjzts@users.noreply.github.com>
2024-12-15 21:58:05 +0800

    test🚦:/ 测试服务器部署 * 5

--- ecommerce-backend/config/environments/development.rb
+++ ecommerce-backend/config/environments/development.rb
@@ -2,7 +2,9 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.default_url_options = { host: "localhost", port: 3000 }
+  # config.default_url_options = { host: "localhost", port: 3000 }
+  config.hosts << "22373058.project.rubyapp.act.buaa.edu.cn:9000"
+
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
